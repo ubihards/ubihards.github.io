d27f658873aed87144f3297344760f5a7070aef7
ubihards <ranzzs108@gmail.com>
2026-03-08 21:47:17 +0700

    Update addons.js

--- js/addons.js
+++ js/addons.js
@@ -8,18 +8,8 @@ page:"addons/daelcraleth.html",
 thumbnail:"./images/dael_banner.png",
 
 tags:["weapon","rpg","armor","combat","custom animation"]
-
-},
-
-{
-title:"Test Addon",
-
-page:"addons/magic-sword.html",
-
-thumbnail:"./images/ubihard_frostbound_hammer.png",
-
-tags:["weapon","magic"]
-
+  
 }
+
 
 ];
