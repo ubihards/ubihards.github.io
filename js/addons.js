fd8e5701f361d5b83d5cc2bd066ae9f595da9b00
ubihards <ranzzs108@gmail.com>
2026-03-08 21:32:27 +0700

    Add files via upload

---
+++ js/addons.js
@@ -0,0 +1,25 @@
+const addons = [
+
+{
+title:"Daelcraleth Blade Addon",
+
+page:"addons/daelcraleth.html",
+
+thumbnail:"./images/dael_banner.png",
+
+tags:["weapon","rpg","armor","combat","custom animation"]
+
+},
+
+{
+title:"Test Addon",
+
+page:"addons/magic-sword.html",
+
+thumbnail:"./images/ubihard_frostbound_hammer.png",
+
+tags:["weapon","magic"]
+
+}
+
+];
---
+++ js/script.js
@@ -0,0 +1,57 @@
+const container=document.getElementById("addonList");
+
+function renderAddons(list){
+
+container.innerHTML="";
+
+list.forEach(addon=>{
+
+const card=document.createElement("div");
+
+card.className="card";
+
+card.innerHTML=`
+
+<img src="${addon.thumbnail}">
+<div class="card-body">
+<div class="card-title">${addon.title}</div>
+<div class="card-tags">
+${addon.tags.map(t=>`<span>${t}</span>`).join(" ")}
+</div>
+</div>`;
+
+card.onclick=()=>{
+location.href=addon.page;
+};
+
+container.appendChild(card);
+});
+
+}
+renderAddons(addons);
+
+/* SEARCH */
+
+const search=document.getElementById("searchInput");
+search.addEventListener("input",()=>{
+const text=search.value.toLowerCase();
+const filtered=addons.filter(a=>a.title.toLowerCase().includes(text));
+renderAddons(filtered);
+});
+
+/* TAG FILTER */
+
+const tagContainer=document.getElementById("tagFilter");
+const tags=[...new Set(addons.flatMap(a=>a.tags))];
+tags.forEach(tag=>{
+const el=document.createElement("span");
+el.innerText=tag;
+el.onclick=()=>{
+const filtered=addons.filter(a=>a.tags.includes(tag));
+renderAddons(filtered);
+
+};
+
+tagContainer.appendChild(el);
+
+});
